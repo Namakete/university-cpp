bbb4c6a5123d948a427073003b31b075ccd448a0
Ilya Oberemok <namakete.dev@gmail.com>
2022-09-27 23:46:06 +0300

    Need to make your `pow` function.

---
+++ ex.1/src/header/pow.hpp
@@ -0,0 +1,18 @@
+// Write a program that accepts two numbers `a` and `b` from the keyboard
+// and deriving a to the power b. The degree must be given as an integer
+// number and can be negative.
+
+namespace math
+{
+    /* A recursive function to get `x * y`. */
+    double multiply(double x, int y);
+
+    /*
+     * @brief A recursive function to get `a ^ b`.
+     *
+     * @param `base` is value container whose values are
+     * summed of `exponent` times.
+     * @file From `pow.hpp`.
+     */
+    double pow(double base, int exponent);
+}
---
+++ ex.1/src/source/main.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) Namakete (Ilya Oberemok) <namakete.dev@gmail.com>.
+// See the LICENCE file in the repository root for full licence text.
+
+#include "../header/include.hpp"
+#include "../header/pow.hpp"
+#include <cmath>
+
+int main(int argc, char **argv)
+{
+    std::cout << pow(2, 3) << std::endl;
+    std::cout << math::pow(2, 3) << std::endl;
+    //
+    std::cout << pow(2.5, 3) << std::endl;
+    std::cout << math::pow(2.5, 3) << std::endl;
+    //
+    std::cout << pow(2.5, -3) << std::endl;
+    std::cout << math::pow(2.5, -3) << std::endl;
+    return 0;
+}
