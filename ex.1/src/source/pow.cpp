c12902d42419dd40090004b54a0f6f3259388427
Ilya Oberemok <namakete.dev@gmail.com>
2022-09-28 13:09:50 +0300

    Fixed `pow` function.

--- ex.1/src/source/pow.cpp
+++ ex.1/src/source/pow.cpp
@@ -3,16 +3,13 @@ namespace math
     double pow(double base, int exponent){
         double result = 1;
 
-        if(exponent > 0){
-            for (int i = 0; i < exponent; i++){
+        if(exponent > 0)
+            for (int i = 0; i < exponent; i++)
                 result *= base;
-            }
-        }
-        else{
-            for (int i = 0; i < exponent; i--){
-                result /= base;
-            }
-        }
+
+        for (int i = 0; i > exponent; i--)
+            result /= base;
+
         return result;
     }
 }
