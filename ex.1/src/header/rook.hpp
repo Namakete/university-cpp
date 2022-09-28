91f536d2e07c16270acf182e9c87344a26a464d1
Ilya Oberemok <namakete.dev@gmail.com>
2022-09-28 14:04:19 +0300

    Added function to given the coordinates of rook standing.

---
+++ ex.1/src/header/rook.hpp
@@ -0,0 +1,8 @@
+// Given the coordinates of the rook standing on the square of the
+// chessboard (row number and column number). Write a loop (or loops)
+// for displaying the coordinates of all cells under attack. Rook
+// walks on any number of fields horizontally or vertically.
+
+namespace chess {
+    void rook(int DESK_SIZE, int x = 0, int y = 0);
+}
