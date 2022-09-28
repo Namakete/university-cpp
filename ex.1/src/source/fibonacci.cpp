1de7107ab188223e8ce342f537ba558e250d407d
Ilya Oberemok <namakete.dev@gmail.com>
2022-09-28 13:46:08 +0300

    Fixed `fibonacci` function.

--- ex.1/src/source/fibonacci.cpp
+++ ex.1/src/source/fibonacci.cpp
@@ -2,8 +2,14 @@
 
 namespace magic{
     int fibonacci(int value){
-        if (value <= 1)
-            return value;
-        return fibonacci(value - 1) + fibonacci(value - 2);
+        int x = 1;
+        int y = 1;
+
+        while(x < value){
+            x = x + y;
+            y = x - y;
+        }
+
+        return x;
     }
 }
